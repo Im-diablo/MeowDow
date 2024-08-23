826c14fbacb875545986036f7aedb62ce58e8b18
Im-diablo <adit200515@gmail.com>
2024-08-23 21:31:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -91,30 +91,31 @@ async def ping(interaction: discord.Interaction):
 async def say(interaction: discord.Interaction, thing_to_say: str, to: discord.Member): 
     await interaction.response.send_message(f"{interaction.user.mention} said: '{thing_to_say}' to {to.mention}")
 
-@bot.tree.command(name="meow")
-async def meow(interaction: discord.Interaction):
-    await interaction.response.send_message(f"meow😼 {interaction.user.mention}!", ephemeral=True)
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
 
-@bot.tree.command(name="catfact")
-async def catfact(interaction: discord.Interaction):
-    try:
-        response = requests.get("https://catfact.ninja/fact")
-        cat_fact = response.json()["fact"]
-        await interaction.response.send_message(cat_fact)
-    except Exception as e:
-        print(f"Error fetching cat image: {e}")
-        await interaction.response.send_message("Sorry, I could not fetch a cat fact at this time.")
+    if message.content == "!meow":
+        await message.channel.send("Meow! ")
 
-@bot.tree.command(name="nekopic")
-async def necopic(interaction: discord.Interaction):
-    try:
-        response = requests.get("https://api.thecatapi.com/v1/images/search")
-        cat_image_url = response.json()[0]["url"]
-        await interaction.response.send_message(cat_image_url)
-    except Exception as e:
-        print(f"Error fetching cat image: {e}")
-        await interaction.response.send_message("Sorry, I could not fetch a cat image at this time.")
+    if message.content == "!catfact":
+        try:
+            response = requests.get("https://catfact.ninja/fact")
+            cat_fact = response.json()["fact"]
+            await message.channel.send(cat_fact)
+        except Exception as e:
+            print(f"Error fetching cat fact: {e}")
+            await message.channel.send("Sorry, I could not fetch a fact at this time.")
 
+    if message.content == "!nekopic":
+        try:
+            response = requests.get("https://api.thecatapi.com/v1/images/search")
+            cat_image_url = response.json()[0]["url"]
+            await message.channel.send(cat_image_url)
+        except Exception as e:
+            print(f"Error fetching cat image: {e}")
+            await message.channel.send("Sorry, I could not fetch a cat image at this time.")
 
 url = 'https://drive.google.com/u/0/uc?id=1F3ZGuaKN4ugYe_K1k9jLpAndTrNUvyWs'
 output = 'token.txt'
